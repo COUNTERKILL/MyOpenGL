bdf2a3bef7974c85a93e4db3e18f96620bd12eae
COUNTERKILL <COUNTERKILL@inbox.ru>
2015-03-22 21:16:16 +0500

    Upgrade RasterizePoly to to render from texture

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import rasterizator
+import VertexShader as vs
+
+vs.SetLookAtLH(np.array([0, 2, -5], dtype=float), np.array([0, 0, 2], dtype=float), np.array([0, 1, 0], dtype=float))
+vs.SetPerspectiveFovLH(math.pi/4, 1.0, 2.0, 10.0)
+
+
+
+vecInView = np.dot(np.array([-1, -2, 5, 1], dtype=float), MATRIX_VIEW)
--- rasterizator.py
+++ rasterizator.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import Image, ImageDraw, math
 import numpy as np
+TEXTURE = Image.Image()
 
 SCREEN = np.zeros((1024, 1024, 3), dtype=int)
 ZBUFFER = np.zeros((1024, 1024, 3), dtype=float)
@@ -14,11 +15,18 @@ class Vertex:
 		self.z = z
 		self.color = color
 		return
-	x = 0
-	y = 0
-	z = 0
-	texCoordU = 0
-	texCoordV = 0
+	def __init__(self, x, y, z, u, v):
+		self.x = x
+		self.y = y
+		self.z = z
+		self.u = u
+		self.v = v
+		return
+	x = 0.0
+	y = 0.0
+	z = 0.0
+	u = 0.0
+	v = 0.0
 	color = (0, 0, 0)
 #end class Vertex	
 	
@@ -83,14 +91,6 @@ def DrawLine(image, v0, v1):
 #end drawLine
 
 def RasterizationPoly(image, v1, v2, v3):
-	# texCoord is defined
-	if hasattr(v1, "texCoordU"):
-		
-		return
-	
-	
-	
-	# if not texture
 	if v1.y > v2.y:
 		if v1.y > v3.y:
 			vHight = v1
@@ -127,46 +127,65 @@ def RasterizationPoly(image, v1, v2, v3):
 		# low part of poly
 		alpha = (vHight.x - vMiddle.x)/(vHight.y - vMiddle.y)
 		beta = (vHight.x - vLow.x)/(vHight.y - vLow.y)
+		
+		# texCoord is defined
+		if hasattr(v1, "u"):
+			alphaT = (vHight.u - vMiddle.u)/(vHight.v - vMiddle.v)
+			betaT = (vHight.u - vLow.u)/(vHight.v - vLow.v)
+			kT = vHight.v - vMiddle.v
+			k = vHight.y - vMiddle.y
 		for dy in range(0, vHight.y - vMiddle.y):
 			x1 = int(beta * (dy + vMiddle.y - vLow.y)) + vLow.x
 			x2 = int(alpha * dy) + vMiddle.x
+			if hasattr(v1, "u"):
+				x1T = betaT * (dy * (vHight.v - vMiddle.v)/(vHight.y - vMiddle.y) + vMiddle.v - vLow.v) + vLow.u
+				x2T = alphaT * dy * (vHight.v - vMiddle.v)/(vHight.y - vMiddle.y) + vMiddle.u
+				xTdX = abs((x2T -x1T)/(x2-x1+0.001))
 			if x1 > x2:
 				step = -1
 			else:
 				step = 1
+			y = vMiddle.y + dy
 			for x in range(x1, x2, step):
-				y = vMiddle.y + dy
-				
-				v0 = Vertex(0, 0, 0, (0, 0, 0))
-				v0.x = x
-				v0.y = y
-				
-				pV1toV2 = GetPointIntersectionNormalLine(v3, v1, v2)
-				pV2toV3 = GetPointIntersectionNormalLine(v1, v2, v3)
-				pV1toV3 = GetPointIntersectionNormalLine(v2, v1, v3)
-				
-				pTopV1toV2 = GetPointIntersectionNormalLine(v0, v3, pV1toV2) # point intersection perp from pixel to perp from v3 to v1-v2 line. 0_o
-				pTopV2toV3 = GetPointIntersectionNormalLine(v0, v1, pV2toV3)
-				pTopV1toV3 = GetPointIntersectionNormalLine(v0, v2, pV1toV3)
-				
-				distToV1 = math.sqrt(((pTopV2toV3.x - v1.x)**2) + ((pTopV2toV3.y - v1.y)**2))
-				distToV2 = math.sqrt(((pTopV1toV3.x - v2.x)**2) + ((pTopV1toV3.y - v2.y)**2))
-				distToV3 = math.sqrt(((pTopV1toV2.x - v3.x)**2) + ((pTopV1toV2.y - v3.y)**2))
-				
-				distV1toP = math.sqrt(((pV2toV3.x - v1.x)**2) + ((pV2toV3.y - v1.y)**2))
-				distV2toP = math.sqrt(((pV1toV3.x - v2.x)**2) + ((pV1toV3.y - v2.y)**2))
-				distV3toP = math.sqrt(((pV1toV2.x - v3.x)**2) + ((pV1toV2.y - v3.y)**2))
-				
-				colorSet1 = [0,0,0]
-				colorSet2 = [0,0,0]
-				colorSet3 = [0,0,0]
 				colorSet = [0,0,0]
-				for i in range(0, 3): 
-					colorSet1[i] = v1.color[i] * (1.0 - distToV1/distV1toP) # color from 1 vertex
-					colorSet2[i] = v2.color[i] * (1.0 - distToV2/distV2toP) # color from 2 vertex
-					colorSet3[i] = v3.color[i] * (1.0 - distToV3/distV3toP) # color from 3 vertex
-				for i in range(0, 3): 
-					colorSet[i] = colorSet1[i] + colorSet2[i] + colorSet3[i]
+				# texCoord is defined
+				if hasattr(v1, "v"):
+					xT = (x - x1) * xTdX + x1T
+					yT = vMiddle.v + (kT/k) * dy
+					(width, height) = TEXTURE.size
+					iyT = int(height * yT)
+					ixT = int(width * xT)
+					colorSet[0], colorSet[1], colorSet[2]  = TEXTURE.getpixel((ixT, iyT))
+				else:
+					v0 = Vertex(0, 0, 0, (0, 0, 0))
+					v0.x = x
+					v0.y = y
+					
+					pV1toV2 = GetPointIntersectionNormalLine(v3, v1, v2)
+					pV2toV3 = GetPointIntersectionNormalLine(v1, v2, v3)
+					pV1toV3 = GetPointIntersectionNormalLine(v2, v1, v3)
+					
+					pTopV1toV2 = GetPointIntersectionNormalLine(v0, v3, pV1toV2) # point intersection perp from pixel to perp from v3 to v1-v2 line. 0_o
+					pTopV2toV3 = GetPointIntersectionNormalLine(v0, v1, pV2toV3)
+					pTopV1toV3 = GetPointIntersectionNormalLine(v0, v2, pV1toV3)
+					
+					distToV1 = math.sqrt(((pTopV2toV3.x - v1.x)**2) + ((pTopV2toV3.y - v1.y)**2))
+					distToV2 = math.sqrt(((pTopV1toV3.x - v2.x)**2) + ((pTopV1toV3.y - v2.y)**2))
+					distToV3 = math.sqrt(((pTopV1toV2.x - v3.x)**2) + ((pTopV1toV2.y - v3.y)**2))
+					
+					distV1toP = math.sqrt(((pV2toV3.x - v1.x)**2) + ((pV2toV3.y - v1.y)**2))
+					distV2toP = math.sqrt(((pV1toV3.x - v2.x)**2) + ((pV1toV3.y - v2.y)**2))
+					distV3toP = math.sqrt(((pV1toV2.x - v3.x)**2) + ((pV1toV2.y - v3.y)**2))
+					
+					colorSet1 = [0,0,0]
+					colorSet2 = [0,0,0]
+					colorSet3 = [0,0,0]
+					for i in range(0, 3): 
+						colorSet1[i] = v1.color[i] * (1.0 - distToV1/distV1toP) # color from 1 vertex
+						colorSet2[i] = v2.color[i] * (1.0 - distToV2/distV2toP) # color from 2 vertex
+						colorSet3[i] = v3.color[i] * (1.0 - distToV3/distV3toP) # color from 3 vertex
+					for i in range(0, 3): 
+						colorSet[i] = colorSet1[i] + colorSet2[i] + colorSet3[i]
 				color = tuple(int(i) for i in colorSet)
 				SCREEN[x, y] = color
 				
@@ -174,45 +193,66 @@ def RasterizationPoly(image, v1, v2, v3):
 		# hight part of poly
 		alpha = (vMiddle.x - vLow.x)/(vMiddle.y - vLow.y)
 		beta = (vHight.x - vLow.x)/(vHight.y - vLow.y)
+		# texCoord is defined
+		if hasattr(v1, "u"):
+			alphaT = (vMiddle.u - vLow.u)/(vMiddle.v - vLow.v)
+			betaT = (vHight.u - vLow.u)/(vHight.v - vLow.v)
+			kT = vMiddle.v - vLow.v
+			k = vMiddle.y - vLow.y
 		for dy in range(0, vMiddle.y - vLow.y):
 			x1 = int(beta * dy) + vLow.x
 			x2 = int(alpha * dy) + vLow.x
+			if hasattr(v1, "u"):
+				x1T = betaT * (dy * (vMiddle.v - vLow.v)/(vMiddle.y - vLow.y)) + vLow.u
+				x2T = alphaT * dy * (vMiddle.v - vLow.v)/(vMiddle.y - vLow.y) + vLow.u
+				xTdX = abs((x2T - x1T)/(x2-x1+0.001))
+				
 			if x1 > x2:
 				step = -1
 			else:
 				step = 1
 			for x in range(x1, x2, step):
 				y = vLow.y + dy
-				v0 = Vertex(0, 0, 0, (0, 0, 0))
-				v0.x = x
-				v0.y = y
-				
-				pV1toV2 = GetPointIntersectionNormalLine(v3, v1, v2)
-				pV2toV3 = GetPointIntersectionNormalLine(v1, v2, v3)
-				pV1toV3 = GetPointIntersectionNormalLine(v2, v1, v3)
-				
-				pTopV1toV2 = GetPointIntersectionNormalLine(v0, v3, pV1toV2) # point intersection perp from pixel to perp from v3 to v1-v2 line. 0_o
-				pTopV2toV3 = GetPointIntersectionNormalLine(v0, v1, pV2toV3)
-				pTopV1toV3 = GetPointIntersectionNormalLine(v0, v2, pV1toV3)
-				
-				distToV1 = math.sqrt(((pTopV2toV3.x - v1.x)**2) + ((pTopV2toV3.y - v1.y)**2))
-				distToV2 = math.sqrt(((pTopV1toV3.x - v2.x)**2) + ((pTopV1toV3.y - v2.y)**2))
-				distToV3 = math.sqrt(((pTopV1toV2.x - v3.x)**2) + ((pTopV1toV2.y - v3.y)**2))
-				
-				distV1toP = math.sqrt(((pV2toV3.x - v1.x)**2) + ((pV2toV3.y - v1.y)**2))
-				distV2toP = math.sqrt(((pV1toV3.x - v2.x)**2) + ((pV1toV3.y - v2.y)**2))
-				distV3toP = math.sqrt(((pV1toV2.x - v3.x)**2) + ((pV1toV2.y - v3.y)**2))
-				
-				colorSet1 = [0,0,0]
-				colorSet2 = [0,0,0]
-				colorSet3 = [0,0,0]
 				colorSet = [0,0,0]
-				for i in range(0, 3): 
-					colorSet1[i] = v1.color[i] * (1.0 - distToV1/distV1toP) # color from 1 vertex
-					colorSet2[i] = v2.color[i] * (1.0 - distToV2/distV2toP) # color from 2 vertex
-					colorSet3[i] = v3.color[i] * (1.0 - distToV3/distV3toP) # color from 3 vertex
-				for i in range(0, 3): 
-					colorSet[i] = colorSet1[i] + colorSet2[i] + colorSet3[i]
+				# texCoord is defined
+				if hasattr(v1, "v"):
+					xT = (x - x1) * xTdX + x1T
+					yT = vLow.v + (kT/k) * dy
+					(width, height) = TEXTURE.size
+					iyT = int(height * yT)
+					ixT = int(width * xT)
+					colorSet[0], colorSet[1], colorSet[2]  = TEXTURE.getpixel((ixT, iyT))
+				else:
+					v0 = Vertex(0, 0, 0, (0, 0, 0))
+					v0.x = x
+					v0.y = y
+					
+					pV1toV2 = GetPointIntersectionNormalLine(v3, v1, v2)
+					pV2toV3 = GetPointIntersectionNormalLine(v1, v2, v3)
+					pV1toV3 = GetPointIntersectionNormalLine(v2, v1, v3)
+					
+					pTopV1toV2 = GetPointIntersectionNormalLine(v0, v3, pV1toV2) # point intersection perp from pixel to perp from v3 to v1-v2 line. 0_o
+					pTopV2toV3 = GetPointIntersectionNormalLine(v0, v1, pV2toV3)
+					pTopV1toV3 = GetPointIntersectionNormalLine(v0, v2, pV1toV3)
+					
+					distToV1 = math.sqrt(((pTopV2toV3.x - v1.x)**2) + ((pTopV2toV3.y - v1.y)**2))
+					distToV2 = math.sqrt(((pTopV1toV3.x - v2.x)**2) + ((pTopV1toV3.y - v2.y)**2))
+					distToV3 = math.sqrt(((pTopV1toV2.x - v3.x)**2) + ((pTopV1toV2.y - v3.y)**2))
+					
+					distV1toP = math.sqrt(((pV2toV3.x - v1.x)**2) + ((pV2toV3.y - v1.y)**2))
+					distV2toP = math.sqrt(((pV1toV3.x - v2.x)**2) + ((pV1toV3.y - v2.y)**2))
+					distV3toP = math.sqrt(((pV1toV2.x - v3.x)**2) + ((pV1toV2.y - v3.y)**2))
+					
+					colorSet1 = [0,0,0]
+					colorSet2 = [0,0,0]
+					colorSet3 = [0,0,0]
+					
+					for i in range(0, 3): 
+						colorSet1[i] = v1.color[i] * (1.0 - distToV1/distV1toP) # color from 1 vertex
+						colorSet2[i] = v2.color[i] * (1.0 - distToV2/distV2toP) # color from 2 vertex
+						colorSet3[i] = v3.color[i] * (1.0 - distToV3/distV3toP) # color from 3 vertex
+					for i in range(0, 3): 
+						colorSet[i] = colorSet1[i] + colorSet2[i] + colorSet3[i]
 				color = tuple(int(i) for i in colorSet)
 				SCREEN[x, y] = color
 				
@@ -228,6 +268,7 @@ def DrawPoly(image, v1, v2, v3):
 
 
 image = Image.new("RGBA", (1024,1024), (0,0,0,0))
+TEXTURE = Image.open("texture.jpg")
 
 (width, height) = image.size
 
@@ -235,10 +276,10 @@ image = Image.new("RGBA", (1024,1024), (0,0,0,0))
  # image.putpixel((i, i), (0, 255, 0))
 
 color = (0, 0, 255)
-v1 = Vertex(100,100,0,(255, 0, 0))
-v2 = Vertex(1000,200,0,(0, 255, 0))
+v1 = Vertex(10,10,0, 0, 0)
+v2 = Vertex(1000,200,0, 0.9766, 0.1953)
 #DrawLine(image, v1, v2, color)
-v3 = Vertex(500,500,0,(0, 0, 255))
+v3 = Vertex(500,1000,0, 0.4882, 0.9766)
 DrawPoly(image, v1, v2, v3)
 #draw = ImageDraw.Draw(image)
 #draw. ellipse((10,10,300,300), fill="white", outline="red")
@@ -247,9 +288,9 @@ DrawPoly(image, v1, v2, v3)
 
 if hasattr(Vertex, "text"):
 	print 123
-for row in range(0, SCREEN.shape[0]):
-	for col in range(0, SCREEN.shape[1]):
-		image.putpixel((row, col), tuple(SCREEN[row, col]))
+for x in range(0, SCREEN.shape[0]):
+	for y in range(0, SCREEN.shape[1]):
+		image.putpixel((x, y), tuple(SCREEN[x, y]))
 image.show()
 image.save("C:\img.png", "PNG")
 del image
